b2fbb458e1ecdcec53437b45907d5bc756094f6f
ananthu <ananthusmk@gmail.com>
2026-02-26 20:26:16 +0530

    changes made in eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,14 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-wrapper-object-types": "off",
+      "react-hooks/exhaustive-deps": "warn"
+    }
+  },
+  {
     ignores: [
       "node_modules/**",
       ".next/**",
